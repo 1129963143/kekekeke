ee0861097d9c4f1541802876c433ededd23d372b
wzhjbfq3@163.com <wzhjbfq3@163.com>
2016-06-30 18:32:08 +0800

    增加在内容详情页面显示上一条／下一条数据并允许跳转。

--- app/helpers/blogs/posts_helper.rb
+++ app/helpers/blogs/posts_helper.rb
@@ -1,2 +1,17 @@
 module Blogs::PostsHelper
+
+  #根据给出的@post 获取前一条数据
+  def get_previous_one(current_post)
+    raise if current_post.blank?
+    return current_post if Blogs::Post.first.id >= current_post.id
+
+    Blogs::Post.where('id < ?',current_post.id).last
+  end
+  #根据给出的@post获取后一条数据
+  def get_after_one(current_post)
+    return false if current_post.blank?
+    return current_post if Blogs::Post.last.id <= current_post.id
+    Blogs::Post.where('id > ?',current_post.id).limit(1)[0]
+  end
+
 end
